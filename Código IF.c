8f887eb34dd01a8f6ff5e2f67f985bb8a9579ec4
Joel-Lavoisier <djoel5174@gmail.com>
2024-10-08 09:44:03 -0300

    Add files via upload

---
+++ Código IF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h> 
+
+int main(){
+    int idadeFernando = 15;
+    int idadeDanilo = 16;
+
+    if(idadeFernando > idadeDanilo){
+printf("Fernando e mais velho que Danilo!");
+    }
+else printf("Danilo e mais velho que Fernando");
+    
+
+if(idadeFernando == idadeDanilo){
+printf("Ambos tem a mesma idade");
+}
+}
